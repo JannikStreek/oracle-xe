93e4ead1b4b0d3db6426c42c19cfdda95289789b
JannikStreek <theshowcanbegin@gmail.com>
2015-12-18 11:08:47 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,7 @@
 
 # Put the Oracle XE RPM somewhere and list its URL in this attribute. Typically you'll want
 # to do this from within a role
-default['oracle-xe']['url'] = 'http://0.0.0.0:3000'
+default['oracle-xe']['url'] = 'http://10.0.2.2'
 
 default['oracle-xe']['http-port'] = 8079
 default['oracle-xe']['tnslsnr-port'] = 1521
